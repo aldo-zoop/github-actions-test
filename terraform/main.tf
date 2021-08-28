58c0fbb72050b6f558e837fe97a13d77f1a731c6
Aldo Graças <aldo.gracas@zoop.com.br>
2021-08-28 16:26:45 -0300

    path terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+  profile                     = "default"
+  region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
