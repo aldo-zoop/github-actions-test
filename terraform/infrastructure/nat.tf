94f4a94f2175f5b2d6938fe68a28ece306cbb6a0
aldo-zoop <78598591+aldo-zoop@users.noreply.github.com>
2021-09-18 12:17:33 -0300

    Adding ec2 step (#33)

--- terraform/infrastructure/nat.tf
+++ terraform/infrastructure/nat.tf
@@ -13,3 +13,18 @@ resource "aws_route" "nat-gw-route" {
   nat_gateway_id         = aws_nat_gateway.prod-nat-gateway.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+  tags = {
+    Name = "Private Route table"
+  }
+}
+
+resource "aws_eip" "elastic-ip-for-nat-gtw" {
+  vpc                       = true
+  associate_with_private_ip = "10.0.0.5"
+  tags = {
+    Name = "Prod EIP"
+  }
+}
